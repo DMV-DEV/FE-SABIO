5e41f172dc20e478891a60dcabb13d8c77ccefbc
Victoria <victoriamariateran@gmail.com>
2024-06-14 18:57:35 -0300

    arreglos layout base

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -7,6 +7,13 @@ const Sidebar = () => {
     const toggleSidebar = () => {
       setIsCollapsed(!isCollapsed);
     };
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (event) => {
+      setSelectedOption(event.target.value);
+    };
+  
   
     return (
       <>
@@ -15,10 +22,19 @@ const Sidebar = () => {
           </button>
         <aside className={`sidebar ${isCollapsed ? '' : 'collapsed'}`}>
             <div className='sidebar__header'>
-                <img className='sidebar__logo' alt='logo' src='/logo.png'/>
-                <img className='sidebar__name' src='/name.png'/>
+                <img className='sidebar__header--logo' alt='logo' src='/logo.png'/>
+                <img className='sidebar__header--name' src='/name.png'/>
             </div>
-          
+            <div className="dropdown-container">
+      
+      <select id="dropdown" value={selectedOption} onChange={handleChange}>
+        <option value="">Select a class</option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+      </select>
+      
+    </div>
           <ul className='sidebar__list'>
             <li>Home</li>
             <li>About</li>
