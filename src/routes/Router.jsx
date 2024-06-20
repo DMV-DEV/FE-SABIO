08e16132e08d21b4a246bbe646a3341d11b3d850
delfinapazzavalia <delfinapazzavalia@gmail.com>
2024-06-20 17:50:57 -0300

    authentication

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
-import Login from '../pages/login/Login';
 import Profile from '../pages/profile/Profile';
 import Classes from '../pages/MyClasses/Classes';
 import AIChecker from '../pages/AIChecker/AIChecker';
@@ -12,7 +11,6 @@ export const router = createBrowserRouter([
         element: <App/>,
         // errorElement: <ErrorPage/>,
         children:[
-            {path: '/Login', element: <Login/>},
             {path: '/myclasses', element: <Classes/>},
             {path: '/account', element: <Profile/>},
             {path: '/checker', element: <AIChecker/>},
