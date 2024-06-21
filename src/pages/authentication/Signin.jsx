f737ff20ac82b00a5a06e47bc9f99de1442b8d08
delfinapazzavalia <delfinapazzavalia@gmail.com>
2024-06-21 11:22:29 -0300

    signin linkeado

--- src/pages/authentication/Signin.jsx
+++ src/pages/authentication/Signin.jsx
@@ -1,51 +1,59 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./authentication.css";
 
 const Signin = () => {
+  const navigate = useNavigate();
+
+  const handleSignIn = () => {
+    
+    navigate('/myclasses');
+  };
+
   return (
     <div className="login__card">
-        <h2 className="login__title">Sign In</h2>
-        <label className="login__label" htmlFor="email">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          className="login__input"
-          placeholder="Enter your email"
-        />
-        <label className="login__label" htmlFor="password">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          className="login__input"
-          placeholder="Enter your password"
-        />
-        <div className="login__options">
-          <div className="login__checkbox-container">
-            <input
-              type="checkbox"
-              id="rememberMe"
-              className="login__checkbox"
-            />
-            <label htmlFor="rememberMe" className="login__checkbox-label">
-              Remember me
-            </label>
-          </div>
-          <button className="button__underlined login__forgot-password button__underlined">
-            Forgot password
-          </button>
+      <h2 className="login__title">Sign In</h2>
+      <label className="login__label" htmlFor="email">
+        Email
+      </label>
+      <input
+        type="email"
+        id="email"
+        className="login__input"
+        placeholder="Enter your email"
+      />
+      <label className="login__label" htmlFor="password">
+        Password
+      </label>
+      <input
+        type="password"
+        id="password"
+        className="login__input"
+        placeholder="Enter your password"
+      />
+      <div className="login__options">
+        <div className="login__checkbox-container">
+          <input
+            type="checkbox"
+            id="rememberMe"
+            className="login__checkbox"
+          />
+          <label htmlFor="rememberMe" className="login__checkbox-label">
+            Remember me
+          </label>
         </div>
-        <button className="login__button">Sign In</button>
-        <p className="login__lorem">
-          By connecting with the services above you agree to our Terms of
-          Services and acknowledge our Privacy Policy describing how we handle
-          your personal data.
-        </p>
+        <button className="button__underlined login__forgot-password button__underlined">
+          Forgot password
+        </button>
       </div>
-  )
-}
+      <button className="login__button" onClick={handleSignIn}>Sign In</button>
+      <p className="login__lorem">
+        By connecting with the services above you agree to our Terms of
+        Services and acknowledge our Privacy Policy describing how we handle
+        your personal data.
+      </p>
+    </div>
+  );
+};
 
-export default Signin
+export default Signin;
