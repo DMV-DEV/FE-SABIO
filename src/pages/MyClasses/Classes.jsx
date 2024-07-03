395246616d01bcb0754a8a08af49dc4fb5176241
Victoria <victoriamariateran@gmail.com>
2024-07-03 11:32:56 -0300

    classes slice y funciones comentadas en myvlasses

--- src/pages/MyClasses/Classes.jsx
+++ src/pages/MyClasses/Classes.jsx
@@ -2,18 +2,32 @@ import React, { useState } from 'react';
 import CardComponent from '../../components/cards/CardComponent';
 import './StyleClasses.css';
 import { useNavigate } from 'react-router-dom';
+import { useGetClassesByEducatorQuery, useAddClassMutation } from '../../redux/classesApi.jsx';
+import { useDispatch, useSelector } from 'react-redux';
 
  
 const Classes = () => {
   const [classSelected, setClassSelected] = useState('');
   const navigate = useNavigate();
+  // const  profesorId =
+  // useSelector((state) => state.user.id);
+  // const { data: classes, error, isLoading } = useGetClassesByEducatorQuery(profesorId);
+  // console.log(data);
+  // const [addClass, { isLoading: isAdding }] = useAddClassMutation();
+
+  // const handleAddClass = async () => {
+  //   try {
+  //     await addClass({ nombre: 'Nueva Clase', id: 10 }).unwrap();
+  //   } catch (error) {
+  //     console.error('Failed to add class:', error);
+  //   }
+  // };
 
   const handleClick = (id) => {
     setClassSelected(id);
     navigate(`/dashboard`)
     // navigate(`/dashboard/${id}`)
   
-    
   };
   console.log(classSelected)
     const data = [
