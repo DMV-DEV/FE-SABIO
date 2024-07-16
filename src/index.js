346f6ac51a58db7b3959604035e55c2f33ba685f
Victoria <victoriamariateran@gmail.com>
2024-07-16 16:30:14 -0300

    arreglo configuracion .env

--- src/index.js
+++ src/index.js
@@ -11,8 +11,11 @@ import { RouterProvider } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './redux/store'
 
+
+
 reportWebVitals();
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
