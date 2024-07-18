40abd62724ec61e46dd69c98f816c06768ead4c1
delfinapazzavalia <delfinapazzavalia@gmail.com>
2024-07-17 22:36:16 -0300

    correc

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,18 +2,25 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './userSlice';
 import { chatApi } from './chatApi'; 
 import { classesApi } from './classesApi';
-import { studentsApi } from './studentsApi'
-
-
-
+import { authApi } from './authApi';
+import { studentsApi } from './studentsApi';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     [chatApi.reducerPath]: chatApi.reducer,
     [classesApi.reducerPath]: classesApi.reducer,
-    [studentsApi.reducerPath]: studentsApi.reducer, 
+    [authApi.reducerPath]: authApi.reducer,
+    [studentsApi.reducerPath]: studentsApi.reducer,
   },
   
-  middleware: (gDM) => gDM().concat(chatApi.middleware,classesApi.middleware, studentsApi.middleware),
+  middleware: (gDM) => gDM().concat(
+    chatApi.middleware, 
+    classesApi.middleware, 
+    authApi.middleware, 
+    studentsApi.middleware
+  ),
 });
+
+setupListeners(store.dispatch);
