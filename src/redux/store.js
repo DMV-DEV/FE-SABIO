21d722156498e95203241e8cfee2f008712e6115
delfinapazzavalia <delfinapazzavalia@gmail.com>
2024-07-17 22:37:50 -0300

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,25 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './userSlice';
 import { chatApi } from './chatApi'; 
 import { classesApi } from './classesApi';
-import { authApi } from './authApi';
-import { studentsApi } from './studentsApi';
-import { setupListeners } from '@reduxjs/toolkit/query';
+import { studentsApi } from './studentsApi'
+
+
+
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     [chatApi.reducerPath]: chatApi.reducer,
     [classesApi.reducerPath]: classesApi.reducer,
-    [authApi.reducerPath]: authApi.reducer,
-    [studentsApi.reducerPath]: studentsApi.reducer,
+    [studentsApi.reducerPath]: studentsApi.reducer, 
   },
   
-  middleware: (gDM) => gDM().concat(
-    chatApi.middleware, 
-    classesApi.middleware, 
-    authApi.middleware, 
-    studentsApi.middleware
-  ),
+  middleware: (gDM) => gDM().concat(chatApi.middleware,classesApi.middleware, studentsApi.middleware),
 });
-
-setupListeners(store.dispatch);
