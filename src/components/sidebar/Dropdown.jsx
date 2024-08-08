5a90679e35482899af4662e6da5be4dbcf33c89e
martinasorialanzi <martinasorialanzi@gmail.com>
2024-08-07 21:23:07 -0300

    student interface, selct classes

--- src/components/sidebar/Dropdown.jsx
+++ src/components/sidebar/Dropdown.jsx
@@ -1,32 +1,55 @@
-import React, { useState } from "react";
-import "./StyleSidebar.css";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useGetClassesByEducatorQuery } from "../../redux/classesApi";
+import React, { useState, useEffect } from 'react';
+import './StyleSidebar.css';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useGetClassesByEducatorQuery } from '../../redux/classesApi';
+import { addClasses } from '../../redux/classesSlice';
 
 const Dropdown = () => {
-  const profesorId = useSelector((state) => state.user.id);
-  const { data, error, isLoading } = useGetClassesByEducatorQuery(profesorId);
   const navigate = useNavigate();
-  const [selectedOption, setSelectedOption] = useState("Select a class");
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const profesorId= 7;
+//   const profesorId = useSelector((state) => state.user.id)
+  const { data: classes = [], error, isLoading } = useGetClassesByEducatorQuery(profesorId);
+  const [selectedOption, setSelectedOption] = useState('Select a class');
 
   const handleChange = (event) => {
-    setSelectedOption(event.target.value);
-    navigate(`/dashboard`);
-    // navigate(`/dashboard/${id}`)
+    const selectedClassId = event.target.value;
+    const selectedClass = classes.find(cls => cls.id === selectedClassId);
+
+    if (selectedClass) {
+      setSelectedOption(selectedClass.nombre);
+      dispatch(addClasses({ nombre: selectedClass.nombre, id: selectedClass.id }));
+
+      if (location.pathname === '/') {
+        navigate(`/dashboard`);
+      }
+    }
   };
+
+  useEffect(() => {
+    if (error) {
+      console.error('Error fetching classes:', error);
+    }
+  }, [error]);
 
   return (
     <div className="sidebar__dropdown">
       <select id="dropdown" value={selectedOption} onChange={handleChange}>
-        <option value="Select a class" disabled>
-          Select a class
-        </option>
-        {data.map((data) => (
-          <option value={data.id}>{data.nombre} </option>
-        ))}
+        <option value="Select a class" disabled>Select a class</option>
+        {isLoading ? (
+          <option value="loading">Loading...</option>
+        ) : (
+          classes.map(cls => (
+            <option key={cls.id} value={cls.id}>
+              {cls.nombre}
+            </option>
+          ))
+        )}
       </select>
     </div>
   );
 };
+
 export default Dropdown;
