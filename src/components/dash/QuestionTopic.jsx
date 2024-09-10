f5b59d226a263d82c838ca8f6e252cbd1102ca96
“delfina” <delfinapazzavalia@gmail.com>
2024-09-10 11:07:21 -0300

    corregido

--- src/components/dash/QuestionTopic.jsx
+++ src/components/dash/QuestionTopic.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
-import { Button } from "antd";
 import TopicAnalysisModal from "./TopicAnalysisModal";
 import "../../components/dash/questionTopic.css";
 
