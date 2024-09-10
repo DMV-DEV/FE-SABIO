7ff9e5525032181331bbe674b95690c22d8c6b41
“delfina” <delfinapazzavalia@gmail.com>
2024-09-10 11:06:47 -0300

    modal agregado

--- src/components/dash/QuestionTopic.jsx
+++ src/components/dash/QuestionTopic.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import React, { useState } from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+import { Button } from "antd";
+import TopicAnalysisModal from "./TopicAnalysisModal";
 import "../../components/dash/questionTopic.css";
 
 const data = [
@@ -12,22 +14,34 @@ const data = [
 ];
 
 const QuestionTopic = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <div className="bar-chart-container">
-    <h3>Question Topic Distribution</h3>
-    <ResponsiveContainer width="100%" height="90%">
-      <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="Blue" fill="#007ed6" />
-        <Bar dataKey="Black" fill="#013e69" />
-      </BarChart>
-    </ResponsiveContainer>
-  </div>
+      <h3>Question Topic Distribution</h3>
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Blue" fill="#007ed6" onClick={showModal} />
+          <Bar dataKey="Black" fill="#013e69" onClick={showModal} />
+        </BarChart>
+      </ResponsiveContainer>
+      <TopicAnalysisModal visible={isModalVisible} onClose={handleCloseModal} />
+    </div>
   );
 };
 
 export default QuestionTopic;
+
